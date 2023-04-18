f83aaa12e901c775dd1932df9ec181e99d140608
lukasz <lukasz.pecherczyk@protonmail.com>
2023-04-18 09:13:26 +0200

    Szablon strony 1.0

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// const inputElement: HTMLInputElement | null = document.querySelector('#drips')
+// const button: HTMLInputElement | null = document.querySelector('#run')
+
+// if (inputElement != null) {
+//   if (button != null) {
+//     button.addEventListener('click', () => {
+//       console.log(inputElement.value)
+//     })
+//   }
+// }
